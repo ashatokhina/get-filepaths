80597ec77e7fb45f29f1cf1d1dee4a12637a07d5
Sasha Shatokhina <shopot@live.ru>
2021-03-22 00:12:25 +0300

    🎉Initial commit

---
+++ Get-FilePaths/Get-FilePaths.psm1
@@ -0,0 +1,48 @@
+﻿function Get-FilePaths {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)] 
+        [string]$Filemask
+    )
+
+    PROCESS {
+        ForEach ($item in (Get-ChildItem -Path ./$Filemask)) {
+            Write-Host $item.FullName
+        }
+    }
+}
+
+Export-ModuleMember -Function Get-FilePaths
+
+<#
+.SYNOPSIS
+Вывод путей файлов в консоль с заданной маской.
+
+.DESCRIPTION
+Простая утилита для облегчения жизни пользователям Pureref.
+
+.PARAMETER Filemask
+Name of the computer that is failing.
+
+.EXAMPLE
+Get-FilePaths -Filemask IMG_*
+
+.EXAMPLE
+Get-FilePaths -Filemask *.png
+
+.INPUTS
+System.String
+
+Принимает строку как аргумент
+
+.OUTPUTS
+Выводит список полных путей файлов в консоль.
+
+.NOTES
+FunctionName : Get-FilePaths
+Created by   : Sasha Shatokhina
+Date Coded   : 11/11/2020
+
+.LINK 
+Out-File
+#>
